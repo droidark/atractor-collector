2f2e43b47f36c4360892d130a71d2022704919bb
droidark <droidark@outlook.com>
2023-02-11 14:16:44 -0600

    Simple demo with JPA and MySQL

---
+++ src/main/java/io/atractor/comic/collector/domain/model/Publisher.java
@@ -0,0 +1,32 @@
+package io.atractor.comic.collector.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "atr_publisher")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Publisher {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "description")
+	private String description;
+}
---
+++ src/main/java/io/atractor/comic/collector/domain/repository/PublisherRepository.java
@@ -0,0 +1,9 @@
+package io.atractor.comic.collector.domain.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import io.atractor.comic.collector.domain.model.Publisher;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Integer>{
+
+}
